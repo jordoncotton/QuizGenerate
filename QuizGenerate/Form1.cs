74d2d7943340e891cf4cfb549f4914a0b13a6d19
jordoncotton1 <jordoncotton1@gmail.com>
2019-03-21 18:33:53 -0500

    declarations

--- QuizGenerate/Form1.cs
+++ QuizGenerate/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -7,31 +8,55 @@ namespace QuizGenerate
     public partial class Form1 : Form
     {
         private object answer1;
-        private bool Value;
-
+        public int QuestionCorrect { get; set; }
         public Action<EventArgs> OK { get; private set; }
 
         public Form1()
         {
+            button1 = new Button();
+            button1.Click += Submit;
             InitializeComponent();
+            initializequiz();
         }
 
         public class Question
         {
             public string QuestionText { get; set; } 
-            public string[] Choices;
-            public Question(string v)
+            public string question;
+            public string[] answers;
+            public bool isCorrect;
+            public string inputAnswer;
+            private int correctIndex;
+            private string questionType;
+            public static string multipleChoice = "MC";
+            public List<Answer> Choices;
+
+            public Question(string q, string[] answersList, string typeOfQuestion, int correctAnswer)
             {
-                
+                question = q;
+                questionType = typeOfQuestion;
+                if (questionType == multipleChoice)
+                {
+                    answers = new string[4];
+                }
+
+                for (int i = 0; i < answersList.Length; i++)
+                {
+                    this.answers[i] = answersList[i];
+                }
+
+                int correctIndex = correctAnswer;
+
+                List<Answer> answers = new List<Answer>();
             }
-            public int Answer { get; set; } 
+            public int Answer { get; set; }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
+        
         private void Submit(object sender, EventArgs e)
         {
             button1.BackColor = Color.Aqua;
@@ -41,9 +66,9 @@ namespace QuizGenerate
 
             if (answer1 == DialogResult.OK)
             {
-                OK = OnClick;
-                Console.ReadKey();
-                Console.Read();
+                
+                DialogResult = Properties.Resources.correctly;
+                DialogResult++;
             }
 
             if (answer1 == DialogResult.Cancel)
@@ -51,11 +76,20 @@ namespace QuizGenerate
                 DialogResult = Properties.Resources.Error;
                 DialogResult++;
             }
+
+            
         }
 
-        private static void Quiz(string[] args)
-        {
+        
 
+        public class Quiz
+        {
+            List<Question> questions = new List<Question>();
+        }
+
+        public class Answer
+        {
+            public string give;
         }
 
         private void Button2_Click(object sender, EventArgs e)
@@ -66,48 +100,35 @@ namespace QuizGenerate
         private void RadioButton1_CheckedChanged_1(object sender, EventArgs e)
         {
 
-            if (answer1 != radioButton1)
-            {
-                return;
-            }
+           
         }
 
         private void RadioButton2_CheckedChanged_1(object sender, EventArgs e)
         {
-            if (answer1 != radioButton2)
-            {
-                return;
-            }
+           
         }
 
         private void RadioButton3_CheckedChanged_1(object sender, EventArgs e)
         {
-            if (answer1 == radioButton3)
-            {
-                return;
-            }
+            
         }
 
         private void RadioButton4_CheckedChanged_1(object sender, EventArgs e)
         {
-            if (answer1 != radioButton4)
-            {
-                 return;
-            }
+            
         }
         //Create question objects that represents the strcuture of the program.
         private void QuestionText(object sender, EventArgs e)
         { 
-            Console.Write(value: Value);
-            int answer2 = Convert.ToInt32(Console.ReadLine());
-            Console.ReadKey();
-            String message1 = (answer2 != 4) ? "Incorrect" : "Correct";
-            Console.WriteLine(message1);
-
-            Question question = new Question("What year is 2002?");
-            Console.WriteLine(question.Answer);
-
-            Question question2 = question;
+          
         }
+
+        private void initializequiz()
+        {
+            //create test
+            new Question("What is 1 + 1 ?", new string[] { "1", "2", "3", "4" }, Question.multipleChoice, 1);
+            new Question("What year is 2002 ?", new string[] { "0202", "2002", "2020", "2200" }, Question.multipleChoice, 1);
+        }
+
     }
 }
