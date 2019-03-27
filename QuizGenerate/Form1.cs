eb755ba35e0847e13650abea8313578fb977b80b
jordoncotton1 <jordoncotton1@gmail.com>
2019-03-26 23:15:09 -0500

    added

--- QuizGenerate/Form1.cs
+++ QuizGenerate/Form1.cs
@@ -8,8 +8,14 @@ namespace QuizGenerate
     public partial class Form1 : Form
     {
         private object answer1;
+       
+        private readonly object questions;
+        private readonly int i;
         public int QuestionCorrect { get; set; }
         public Action<EventArgs> OK { get; private set; }
+        public string QuestionText { get; set; }
+        public string[] Answers { get; set; }
+        public string CorrectAnswer { get; set; }
 
         public Form1()
         {
@@ -19,94 +25,50 @@ namespace QuizGenerate
             RadioButton RadioButton = new RadioButton();
 
             button1 = new Button();
-            button1.Click += Submit;
-            
-        }
-
-        public class Question
-        {
-            public string QuestionText { get; set; } 
-            public string question;
-            public string[] answers;
-            public bool isCorrect;
-            public string inputAnswer;
-            private int correctIndex;
-            private string questionType;
-            public static string multipleChoice = "MC";
-            public List<Answer> Choices;
-
-            public Question(string q, string[] answersList, string typeOfQuestion, int correctAnswer)
-            {
-                question = q;
-                questionType = typeOfQuestion;
-                if (questionType == multipleChoice)
-                {
-                    this.answers = new string[4];
-                }
-
-                for (int i = 0; i < answersList.Length; i++)
-                {
-                    this.answers[i] = answersList[i];
-                }
-
-                int correctIndex = correctAnswer;
-
-                List<Answer> answers = new List<Answer>();
-            }
-            public int Answer { get; set; }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+            button1.Click += Submit; 
         }
         
-        private void Submit(object sender, EventArgs e)
+        public class Question
         {
-            button1.BackColor = Color.Aqua;
+            public string QuestionText { get; set; }
+            public string AnswerA { get; set; }
+            public string AnswerB { get; set; }
+            public string AnswerC { get; set; }
+            public string AnswerD { get; set; }
 
-            DialogResult answer1 = MessageBox.Show("What year is 2002?", "Question 1",
-                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-
-            if (answer1 == DialogResult.OK)
+            string[] questions = new string[]
             {
-                DialogResult = Properties.Resources.correctly;
-                DialogResult++;
+                "What is 9*10?",
+                "What year is 2002?",
+                "What 432 backwards?"
+            };
+
+            string[] answers = new string[]
+            {
+                "09, 90, 9, 0",
+                "2002, 2020, 0202, 0022",
+                "432, 324, 234, 423"
+            };
+
+            string[] quizAnswers = new string[]
+            {
+                "90, 2002, 234"
+            };
+
+            string getSelectedAnswer()
+            {
+                if (radioButton1.Checked)
+                    return radioButton1.Text.ToString();
+                if (radioButton2.Checked)
+                    return radioButton2.Text.ToString();
+                if (radioButton3.Checked)
+                    return radioButton3.Text.ToString();
+                if (radioButton4.Checked)
+                    return radioButton4.Text.ToString();
+                return "";
             }
 
-            if (answer1 == DialogResult.Cancel)
-            {
-                DialogResult = Properties.Resources.Error;
-                DialogResult++;
-            }
-
-            DialogResult Question = default(DialogResult);
-            DialogResult = Question;
-            
-            if(Question == DialogResult)
-            {
-                DialogResult = Question;
-            }
-        }
-
-        public class Quiz
-        {
-            List<Question> questions = new List<Question>();
-            private static Quiz give;
-            private static Answer inputAnswer;
-            private static Question question;
-
-            public static implicit operator Quiz(RadioButton v)
-            {
-                Question question = QuestionText();
-                return give;
-            }
-
-            private static Question QuestionText()
-            {
-                Answer answer = inputAnswer;
-                return question;
-            }
+            public static object Text { get; internal set; }
         }
 
         public class Answer
@@ -115,37 +77,59 @@ namespace QuizGenerate
             Answer answer = new Answer();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+    
+        }
+
+        public class Quiz
+        {
+            List<Question> questions = new List<Question>();
+        }
+
+        private void Submit(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Aqua;
+
+            if(i == 0 && radioButton1.Checked)
+            {
+                MessageBox.Show("Correct");
+            }
+            
+            else
+            {
+                MessageBox.Show("Incorrect");
+            }
+        }
+
         private void RadioButton1_CheckedChanged_1(object sender, EventArgs e)
         {
-            Quiz QuestionEventArgs = radioButton1;
+             
         }
 
         private void RadioButton2_CheckedChanged_1(object sender, EventArgs e)
         {
-            Quiz QuestionEventArgs = radioButton2;            
+                    
         }
 
         private void RadioButton3_CheckedChanged_1(object sender, EventArgs e)
         {
-            Quiz QuestionEventArgs = radioButton3;
+            
         }
 
         private void RadioButton4_CheckedChanged_1(object sender, EventArgs e)
         {
-            Quiz QuestionEventArgs = radioButton4;
+            
         }
         //Create question objects that represents the strcuture of the program.
         private void QuestionText(object sender, EventArgs e)
         {
-            initializequiz();
+          
         }
 
         private void initializequiz()
         {
-            //create test
-            new Question("What year is 2002 ?", new string[] { "0202", "2002", "2020", "2200" }, Question.multipleChoice, 1);
-            new Question("What is 1 + 1 ?", new string[] { "1", "2", "3", "4" }, Question.multipleChoice, 1);
-            new Question("What is the square root of 9 ?", new string[] { "6", "5", "45", "3" }, Question.multipleChoice, 1);
+            
         }
     }
 }
